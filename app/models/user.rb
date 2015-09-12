4c86964fd1a90867e134d8e249cf779e2430053b
Zak Stallings <zakstal@gmail.com>
2015-09-12 19:09:18 -0400

    update log on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 
 	def find_group( group )
 		p "groups: #{self.groups}"
-		p "group users: #{self.groups.users}"
+		p "group users: #{self.groups[0].users}"
 		self.groups.find do |g|
 			g.name === group
 		end
