e29ebf16f049210b472244b4f0016671cec5d13f
Zak Stallings <zakstal@gmail.com>
2015-09-12 16:10:53 -0400

    remove validation for created at and updated at

--- app/models/text_in.rb
+++ app/models/text_in.rb
@@ -1,3 +1,3 @@
 class TextIn < ActiveRecord::Base
-	validates  :to_country, :to_state, :sms_message_sid, :num_media, :to_city, :from_zip, :sms_sid, :from_state, :sms_status, :from_city, :body, :from_country, :to, :to_zip, :message_sid, :from, :api_version, :account_sid, :created_at, :updated_at, presence: true
+	validates  :to_country, :to_state, :sms_message_sid, :num_media, :to_city, :from_zip, :sms_sid, :from_state, :sms_status, :from_city, :body, :from_country, :to, :to_zip, :message_sid, :from, :api_version, :account_sid, presence: true
 end
